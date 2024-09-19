90d3fd20bb73ea10ddb666606e7fb4f9d7f214f2
geektao1024 <471787092@qq.com>
2024-09-19 12:18:35 +0800

    更新站点地图

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: process.env.SITE_URL || 'https://www.learncursor.com', // 替换为您的网站 URL
+    siteUrl: process.env.SITE_URL || 'https://www.learn-cursor.com', // 替换为您的网站 URL
     generateRobotsTxt: true,
     changefreq: 'daily',
     priority: 0.7,
@@ -8,7 +8,7 @@ module.exports = {
     exclude: ['/server-sitemap.xml'], // 排除动态生成的 sitemap
     robotsTxtOptions: {
       additionalSitemaps: [
-        'https://www.learncursor.com/server-sitemap.xml', // 添加动态生成的 sitemap
+        'https://www.learn-cursor.com/server-sitemap.xml', // 添加动态生成的 sitemap
       ],
     },
   }
